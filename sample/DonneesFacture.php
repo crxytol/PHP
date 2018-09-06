3651c37356ba32f95dcaf437a146edae364d9817
Ange Michael Atse <michael.atse@ci.12thwonder.com>
2018-09-06 18:39:40 +0000

    Project implementation
    06/09/2018

---
+++ sample/DonneesFacture.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Sample model  soap client 
+ * DonneesFacture.
+ *
+ * Authoor : Ange Michael Y. Atsé
+ *
+ * @since  06/09/2018
+ */
+class DonneesFacture 
+{
+	
+	public $referenceclient;
+    public $adresseemail ;
+    public $numerotelephone;
+    public $numerocompteclient ;
+    public $reglageclient ;
+    public $puissanceclient;
+    public $usageclient ;
+    public $numerofacture;
+    public $regroupementclient;
+    public $datelimite;
+    public $datenouvelindex;
+    public $dateancienindex;
+    public $consommation ;
+    public $nouvelindex ;
+    public $ancienindex ;
+    public $numerocompteur;
+    public $nomclient ;
+    public $montantfacture ;
+}
